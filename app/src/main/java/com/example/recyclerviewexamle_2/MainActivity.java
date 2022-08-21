6fdb0c8aa3590cbab98ab79dbc97ba7337594d7b
Sardor <sardor.rustamov.a@gmail.com>
2022-08-21 13:25:39 +0500

    Edit

--- app/src/main/java/com/example/recyclerviewexamle_2/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewexamle_2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.recyclerviewexamle_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -17,22 +18,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         List<RecyclerModel> models = new ArrayList<>();
-        models.add(new RecyclerModel("Title1", "Desc1"));
-        models.add(new RecyclerModel("Title2", "Desc2"));
-        models.add(new RecyclerModel("Title3", "Desc3"));
-        models.add(new RecyclerModel("Title4", "Desc4"));
-        models.add(new RecyclerModel("Title5", "Desc5"));
-        models.add(new RecyclerModel("Title6", "Desc6"));
-        models.add(new RecyclerModel("Title7", "Desc7"));
-        models.add(new RecyclerModel("Title8", "Desc8"));
-        models.add(new RecyclerModel("Title9", "Desc9"));
+        models.add(new RecyclerModel("Title1", "Description1"));
+        models.add(new RecyclerModel("Title2", "Description2"));
+        models.add(new RecyclerModel("Title3", "Description3"));
+        models.add(new RecyclerModel("Title4", "Description4"));
+        models.add(new RecyclerModel("Title5", "Description5"));
+        models.add(new RecyclerModel("Title6", "Description6"));
+        models.add(new RecyclerModel("Title7", "Description7"));
+        models.add(new RecyclerModel("Title8", "Description8"));
+        models.add(new RecyclerModel("Title9", "Description9"));
+        models.add(new RecyclerModel("Title10", "Description10"));
+        models.add(new RecyclerModel("Title11", "Description11"));
+        models.add(new RecyclerModel("Title12", "Description12"));
+        models.add(new RecyclerModel("Title13", "Description13"));
+        models.add(new RecyclerModel("Title14", "Description14"));
+        models.add(new RecyclerModel("Title15", "Description15"));
+        models.add(new RecyclerModel("Title16", "Description16"));
+        models.add(new RecyclerModel("Title17", "Description17"));
+        models.add(new RecyclerModel("Title18", "Description18"));
+        models.add(new RecyclerModel("Title19", "Description19"));
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         RecyclerAdapter adapter = new RecyclerAdapter();
         adapter.setList(models);
-        GridLayoutManager ln = new GridLayoutManager(this, 3);
+        LinearLayoutManager ln = new LinearLayoutManager(this);
 
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(ln);
+
+        recyclerView.setNestedScrollingEnabled(false);
     }
 }
